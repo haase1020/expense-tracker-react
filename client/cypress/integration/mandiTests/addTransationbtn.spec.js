6d09a2cbc153c7b0b5e4788478a7811419da4173
Jon Haase <haase1020@gmail.com>
2020-08-31 05:41:42 -0400

    add testing

---
+++ client/cypress/integration/mandiTests/addTransationbtn.spec.js
@@ -0,0 +1,9 @@
+describe('adds a transaction to expense list', function () {
+  // arrange
+  it('visits a new site', function () {
+    //act
+    cy.visit('http://localhost:3000');
+    cy.url().should('include', 'add transaction');
+    cy.get('[data-cy=addTransactionButton]').click();
+  });
+});
--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react'
+import React, { useState, useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
@@ -7,35 +7,45 @@ export const AddTransaction = () => {
 
   const { addTransaction } = useContext(GlobalContext);
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     const newTransaction = {
       id: Math.floor(Math.random() * 100000000),
       text,
-      amount: +amount
-    }
+      amount: +amount,
+    };
 
     addTransaction(newTransaction);
-  }
+  };
 
   return (
     <>
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
-        <div className="form-control">
-          <label htmlFor="text">Text</label>
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        <div className='form-control'>
+          <label htmlFor='text'>Text</label>
+          <input
+            type='text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder='Enter text...'
+          />
         </div>
-        <div className="form-control">
-          <label htmlFor="amount"
-            >Amount <br />
-            (negative - expense, positive - income)</label
-          >
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+        <div className='form-control'>
+          <label htmlFor='amount'>
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type='number'
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder='Enter amount...'
+          />
         </div>
-        <button className="btn">Add transaction</button>
+        <button className='btn'>add transaction</button>
       </form>
     </>
-  )
-}
+  );
+};
